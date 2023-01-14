72c122c801d904b8954610b930d649ecd1a21aef
Fabio <fabiote2003@gmail.com>
2023-01-14 15:30:09 -0300

    Probando

--- app.js
+++ app.js
@@ -5,4 +5,11 @@ console.log("Hola")
 
 
 const server = new Server();
+server.app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://fabio-todo-app.netlify.app/');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    res.header('Allow', 'GET, PUT, POST, DELETE, OPTIONS');
+    next();
+  });
 server.listen();
--- models/server.js
+++ models/server.js
@@ -27,13 +27,13 @@ class Server {
     }
 
     middlewares() {
-        this.app.use((req, res, next) => {
-            res.header('Access-Control-Allow-Origin', 'https://fabio-todo-app.netlify.app/');
-            res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Access-Control-Allow-Request-Method');
-            res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-            res.header('Allow', 'GET, PUT, POST, DELETE, OPTIONS');
-            next();
-          });
+        // this.app.use((req, res, next) => {
+        //     res.header('Access-Control-Allow-Origin', 'https://fabio-todo-app.netlify.app/');
+        //     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Access-Control-Allow-Request-Method');
+        //     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+        //     res.header('Allow', 'GET, PUT, POST, DELETE, OPTIONS');
+        //     next();
+        //   });
         //Lectura y parseo del body
         this.app.use(express.json())
 

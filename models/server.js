f8ba9faa7c7fd288234f8099547d758fce3198d1
Fabio <fabiote2003@gmail.com>
2023-01-14 15:22:50 -0300

    Actualizando CORS de nuevo

--- models/server.js
+++ models/server.js
@@ -28,7 +28,7 @@ class Server {
 
     middlewares() {
         this.app.use((req, res, next) => {
-            res.header('Access-Control-Allow-Origin', 'https://todo-app-fabio.netlify.app/');
+            res.header('Access-Control-Allow-Origin', 'https://fabio-todo-app.netlify.app/');
             res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Access-Control-Allow-Request-Method');
             res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
             res.header('Allow', 'GET, PUT, POST, DELETE, OPTIONS');
